d76f39fd784d325cd5dab9c77917d23169bfcfa2
freddyhard <uwhamme@gmail.com>
2014-07-19 23:27:13 +0100

    Create states.py

---
+++ states.py
@@ -0,0 +1,8 @@
+
+class States():
+    def __init__(self):
+        self.FORMATION = 0
+        self.LAUNCHING = 1
+        self.DIVING = 2
+        self.LANDING = 3
+        self.FORMATION_DIVING = 4
